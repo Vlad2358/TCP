9aac9b72157d7e411a308e86fde9ba4bb54a393e
Vlad2358 <107256377+Vlad2358@users.noreply.github.com>
2022-06-10 15:33:26 +0300

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,99 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace ChatClient
+{
+    class Program
+    {
+        static string userName;
+        static string host;
+        static int port;
+        static TcpClient client;
+        static NetworkStream stream;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свое имя: ");
+            userName = Console.ReadLine();
+            Console.Write("Введите IP: ");
+            host = Console.ReadLine();
+            var isNotOkay = true;
+            while (isNotOkay)
+            {
+                try
+                {
+                    Console.Write("Введите порт: ");
+                    port = int.Parse(Console.ReadLine());
+                    isNotOkay = false;
+                }
+                catch
+                {
+                    Console.WriteLine("Введите корректный порт");
+                }
+            }
+            client = new TcpClient();
+            try
+            {
+                client.Connect(host, port);
+                stream = client.GetStream();
+                Thread receiveThread = new Thread(new ThreadStart(ReceiveMessage));
+                receiveThread.Start();
+                SendMessage();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+        static void SendMessage()
+        {
+            Console.WriteLine("Введите сообщение: ");
+
+            while (true)
+            {
+                string message = Console.ReadLine();
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+            }
+        }
+        static void ReceiveMessage()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] data = new byte[64];
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    string message = builder.ToString();
+                    Console.WriteLine(message);
+                }
+                catch
+                {
+                    Console.WriteLine("Подключение прервано!");
+                    Console.ReadLine();
+                    Disconnect();
+                }
+            }
+        }
+        static void Disconnect()
+        {
+            if (stream != null)
+                stream.Close();
+            if (client != null)
+                client.Close();
+            Environment.Exit(0);
+        }
+    }
+}
